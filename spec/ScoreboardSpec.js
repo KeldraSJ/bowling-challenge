d075919f0027d325dce8ca2362881d6820abd4ee
Keldra <KeldraSJ@gmail.com>
2021-10-01 17:17:32 +0100

    added roll function to change score value

--- spec/ScoreboardSpec.js
+++ spec/ScoreboardSpec.js
@@ -8,4 +8,15 @@ describe("Scoreboard", () => {
   it("starts with the first frame", () => {
     expect(scoreboard.frame).toEqual(1);
   });
+
+  it("starts with a score of zero", () => {
+    expect(scoreboard.score).toEqual(0);
+  });
+
+  describe("Roll", () => {
+    it("each roll should add to the scorecard", () => {
+      scoreboard.roll(5);
+      expect(scoreboard.score).toEqual(5);
+    });
+  });
 });
--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -1,5 +1,14 @@
 class Scoreboard {
   constructor(scoreboard) {
     this.frame = 1;
+    this.score = 0;
+  }
+
+  roll(pins) {
+    return (this.score += pins);
   }
 }
+
+let scoreboard = new Scoreboard();
+
+// console.log(scoreboard.roll(10));
